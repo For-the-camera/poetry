7c5b0083f27afc718ac5a5347a72cb0da62c531e
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2023-04-25 13:33:07 +0800

    style: question stem style

--- src/ppt/Page2.vue
+++ src/ppt/Page2.vue
@@ -1,21 +1,31 @@
 <script>
-import PageTitle from '../components/PageTitle.vue';
+import PageTitle from "../components/PageTitle.vue";
 export default {
-    name: "Page2",
-    comments: {
-        PageTitle
-    },
-    props: {},
-    data() {
-        return {};
-    },
-    methods: {},
-    components: { PageTitle }
+  name: "Page2",
+  comments: {
+    PageTitle,
+  },
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  components: { PageTitle },
 };
 </script>
 <template>
   <div class="page-container">
     <PageTitle>2/9</PageTitle>
+    <div class="stem">
+      <p>
+        &emsp;&emsp;一般而言，题材相似的古代诗歌表达的思想感情基本也是相似的。因此，每个小组首先需要从整体上把握诗歌的类型。你的组员已经收集筛选了一些诗歌（
+        <strong>点击右边【诗歌素材】</strong>
+        ），请你继续完成以下几个任务：
+      </p>
+    </div>
   </div>
 </template>
-<style scoped></style>
+<style scoped lang="scss">
+@import "../assets/questionStem.scss";
+
+</style>
